119863002b075c359b4f3b300da253270b480022
basodam <basodam@hva.nl>
2023-09-10 17:13:36 +0200

    Login en Registratie

--- Project_security/main/urls.py
+++ Project_security/main/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 
 from . import views
+from . import forms
 
 urlpatterns = [
     path("", views.home, name="home"),
+    path("login/", views.loginPage, name="login"),
+    path("register/", views.register, name="register"),
 
 ]
--- Project_security/main/views.py
+++ Project_security/main/views.py
@@ -1,6 +1,51 @@
+from django.shortcuts import render, redirect
+from django import forms
 from django.shortcuts import render
 from django.http import HttpResponse
+
+from django.contrib.auth import authenticate, login, logout
+
+from django.contrib import messages
+
+from django.contrib.auth.forms import UserCreationForm
 # Create your views here.
+from .models import *
+from .forms import OrderForm, CreateUserForm
+
+
+
 
 def home(request):
     return render(request, "home.html")
+
+
+def register(request):
+    form = CreateUserForm()
+    
+    if request.method == 'POST':
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Account is succesvol aangemaakt')
+
+            return redirect('login')
+
+
+
+    context = {'form':form}
+    return render(request, "register.html", context)
+
+def loginPage(request):
+
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+
+
+    return render(request, "login.html")
