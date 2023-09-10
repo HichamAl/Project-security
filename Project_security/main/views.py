cf8d27f3f6d37976a59bb4cb28e4abbd00939f99
Hicham <hicham.almakroudi@hva.nl>
2023-09-10 15:13:08 +0200

    home pagina toegevoegd

--- Project_security/main/views.py
+++ Project_security/main/views.py
@@ -2,6 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
-
-def index(response):
-    return HttpResponse("<h1> dit is een test </h1>")
+def home(request):
+    return render(request, "home.html")
