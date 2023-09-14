efca046c744e93aa34c94d9223671d3ec7a287ec
kevin <inkoomk@ad.hva.nl>
2023-09-14 13:19:09 +0200

    Teruggedraait

--- Project_security/Project_security/settings.py
+++ Project_security/Project_security/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'main',
+    
 ]
 
 MIDDLEWARE = [
@@ -127,4 +127,3 @@ STATICFILES_DIRS = [
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-AUTH_USER_MODEL = 'main.CustomUser'
--- Project_security/Project_security/urls.py
+++ Project_security/Project_security/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
 
     path('', include("main.urls")),
     path('admin/', admin.site.urls),
+    path('', include("django.contrib.auth.urls"))
 ]
