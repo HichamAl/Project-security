f8554377ccda570ae70e220b356147d32de40c49
kevin <inkoomk@ad.hva.nl>
2023-09-22 17:01:10 +0200

    admin page update

--- Project_security/register/admin.py
+++ Project_security/register/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from .models import VerifiedUsers
 # Register your models here.
+
+
+class VerifiedUsersAdmin(admin.ModelAdmin):
+    list_display = ('user', 'verified')
+    list_filter = ('verified',)
+    search_fields = ('user__username',)
+
+admin.site.register(VerifiedUsers, VerifiedUsersAdmin)
